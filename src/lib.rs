0b7665141a34e92d3b8eaf3be173887394e25b23
Yehonatan Cohen Scaly <scalys7@gmail.com>
2024-01-24 17:56:23 +0200

    Fixed

--- src/lib.rs
+++ src/lib.rs
@@ -293,7 +293,6 @@ pub trait AdditivelyHomomorphicDecryptionKeyShare<
     /// Correct decryption isn't assured upon success,
     /// and one should be able to verify the output independently or trust the process was done correctly.
     fn combine_decryption_shares_semi_honest(
-        encryption_key: &Self,
         decryption_shares: HashMap<PartyID, Self::DecryptionShare>,
         lagrange_coefficients: HashMap<PartyID, Self::LagrangeCoefficient>,
         public_parameters: &Self::PublicParameters,
@@ -303,7 +302,6 @@ pub trait AdditivelyHomomorphicDecryptionKeyShare<
     /// Maliciously-secure variant in which the corresponding zero-knowledge proofs are verified,
     /// and correct decryption is assured upon success.
     fn combine_decryption_shares(
-        encryption_key: &Self,
         ciphertexts: Vec<EncryptionKey::CiphertextSpaceGroupElement>,
         decryption_shares_and_proofs: HashMap<
             PartyID,
